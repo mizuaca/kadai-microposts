c43f8cbfd5db2630f6c2684fbf001610fcc6c370
mizuaca <mizuaca@gmail.com>
2017-12-19 07:29:16 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,11 @@ Route::get('/', function () {
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 //『「sign up」にアクセスすると「AuthController」の「postRegister」処理を実行する』
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+//ログイン認証用
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
+
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
